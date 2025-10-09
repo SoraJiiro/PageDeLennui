d8545ac60f1d51d7ea774b8b329fcf51de7928c9
ABDELLAH Kheir-eddine <kheireddinedu51@hotmail.fr>
2025-10-09 13:05:47 +0200

    FIXED git add .!

--- Public/js/horloge.js
+++ Public/js/horloge.js
@@ -1,23 +1,25 @@
-const display = document.querySelector("h1");
+document.addEventListener("DOMContentLoaded", () => {
+  const display = document.querySelector("h1");
 
-function horloge() {
-  let now = new Date();
-  let h = now.getHours();
-  let m = now.getMinutes();
-  let s = now.getSeconds();
+  function horloge() {
+    let now = new Date();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
 
-  if (h < 10) {
-    h = "0" + h;
+    if (h < 10) {
+      h = "0" + h;
+    }
+    if (m < 10) {
+      m = "0" + m;
+    }
+    if (s < 10) {
+      s = "0" + s;
+    }
+
+    display.textContent = `[${h}:${m}:${s}]`; // Affichage
   }
-  if (m < 10) {
-    m = "0" + m;
-  }
-  if (s < 10) {
-    s = "0" + s;
-  }
 
-  display.textContent = `[${h}:${m}:${s}]`; // Affichage
-}
-
-horloge();
-setInterval(horloge, 1000);
+  horloge();
+  setInterval(horloge, 1000);
+});
