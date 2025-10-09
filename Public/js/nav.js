48ad1ab6f0a98f5671d349cfab5ac86c2f32a460
ABDELLAH Kheir-eddine <kheireddinedu51@hotmail.fr>
2025-10-09 13:03:05 +0200

    FIXED git add .!

---
+++ Public/js/nav.js
@@ -0,0 +1,29 @@
+document.addEventListener("click", () => {
+  function goTo(section) {
+    document
+      .querySelector("#" + section)
+      .scrollIntoView({ behavior: "smooth" });
+  }
+
+  const sec1 = document.querySelector(".sec1");
+  const sec2 = document.querySelector(".sec2");
+  const sec3 = document.querySelector(".sec3");
+  const sec4 = document.querySelector(".sec4");
+  const sec5 = document.querySelector(".sec5");
+
+  sec1.addEventListener("click", () => {
+    goTo("stage1");
+  });
+  sec2.addEventListener("click", () => {
+    goTo("stage2");
+  });
+  sec3.addEventListener("click", () => {
+    goTo("stage3");
+  });
+  sec4.addEventListener("click", () => {
+    goTo("stage4");
+  });
+  sec5.addEventListener("click", () => {
+    goTo("stage5");
+  });
+});
